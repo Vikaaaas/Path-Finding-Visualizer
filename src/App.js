dd6e63c93367ae6f43d5dee99b285029fb5754f5
Vikas Singh Bhadoriya <vs6889498@gmail.com>
2022-06-24 23:46:52 -0700

    fist commit 25-05-2022

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Path Finding Visualizer</h2>
+      <PathfindingVisualizer/>
+    </div>
+  );
+}
+
+export default App;
